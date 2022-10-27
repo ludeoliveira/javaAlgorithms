4ebe1eba2da50fa7ad7a3462d46f2cbea7f021bf
Lu de Oliveira <luludeolive.dev@gmail.com>
2022-10-27 09:42:48 -0300

    adds matrix exercise

--- src/Matriz1.java
+++ src/Matriz1.java
@@ -3,64 +3,41 @@ import java.util.Scanner;
 
 public class Matriz1 {
 
-//	8) Tem-se um conjunto de dados contendo a altura e o sexo (M, F) de N pessoas. Fazer um programa que calcule e escreva:
-//		a maior e a menor altura do grupo
-//		a média de altura das mulheres
-//		o número de homens
 	
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 
 		int N = sc.nextInt();
-		double[] altura = new double[N];
-		char[] sexo = new char[N];
+		int[][] mat = new int[N][N];
 
 		
 		for (int i = 0; i < N; i++) {
-			altura[i] = sc.nextDouble();
-			sexo[i] = sc.next().charAt(0);
-		}
-		
-		int numeroMulheres = 0;
-		int numeroHomens = 0;
-		
-		for (int i = 0; i < N; i++) {
-			if (sexo[i] == 'F') {
-				numeroMulheres++;
-			} else {
-				numeroHomens++;
-			}	
-		}
-		
-		double somaAlturaMulheres = 0.0;
-		
-		for (int i = 0; i < N; i++) {
-			if (sexo[i] == 'F') {
-				somaAlturaMulheres += altura[i];
+			for (int j = 0; j <N; j++) {
+				mat[i][j] = sc.nextInt();
 			}
 		}
 		
-		double media = (double) somaAlturaMulheres / numeroMulheres;
+		System.out.println("DIAGONAL PRiNCIPAL: ");
 		
-		double menorAltura = altura[0];
-		double maiorAltura = altura[0];
-		 for (int i = 1; i < N; i++) {
-			 if (altura[i] < menorAltura) {
-				 menorAltura = altura[i];
-			 }
-		 }
-		 
-		 for (int i = 1; i < N; i++) {
-			 if (altura[i] > maiorAltura) {
-				 maiorAltura = altura[i];
-			 }
-		 }
+		for (int i = 0; i < N; i++) {
+			System.out.print(mat[i][i] + " ");
+		}
+		System.out.println();
 		
-		System.out.println("Menor altura = " + menorAltura);
-		System.out.println("Maior altura = " + maiorAltura);
-		System.out.println("Numero de homens = " + numeroHomens);
-		System.out.printf("Media das alturas das mulheres = %.2f%n", media);
+		int cont = 0;
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if (mat[i][j] < 0) {
+					cont++;
+				}
+			}	
+		}
+		
+		System.out.println("QUANTIDADE DE NEGATIVOS: " + cont);
+		
+
 		
 		sc.close();
 	}
